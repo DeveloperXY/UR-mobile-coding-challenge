58c43da704f2eddf76a600c095bccd33c79fbd93
Mohammed Aouf Zouag <m.a.zouag@gmail.com>
2019-04-26 10:49:02 +0100

    Set up an MVP structure.

---
+++ app/src/main/java/com/developerxy/githubapp/MainActivityContract.kt
@@ -0,0 +1,14 @@
+package com.developerxy.githubapp
+
+/**
+ * Created by Mohammed Aouf ZOUAG on 4/26/2019.
+ */
+interface MainActivityContract {
+    interface View {
+
+    }
+
+    interface Presenter {
+        fun start()
+    }
+}
---
+++ app/src/main/java/com/developerxy/githubapp/MainActivityPresenter.kt
@@ -0,0 +1,10 @@
+package com.developerxy.githubapp
+
+/**
+ * Created by Mohammed Aouf ZOUAG on 4/26/2019.
+ */
+class MainActivityPresenter(private var mView: MainActivityContract.View): MainActivityContract.Presenter {
+    override fun start() {
+
+    }
+}
